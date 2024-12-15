0101f1f903ce71d8effabc5959d25fb8ef963291
JorgeFVSz <jorge.silvestre@zup.com.br>
2024-12-15 18:34:58 -0300

    feat: add user guess input and scoring logic

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,22 @@ public class Main {
         int numeroSorteado = random.nextInt(10) + 1;
         System.out.println("Número sorteado: " + numeroSorteado);
 
+        int palpiteUsuario = scanner.nextInt();
+
+        if (numeroSorteado == palpiteUsuario) {
+            pontuacao += 10;
+            System.out.println("Parabens você acertou o numero éra " + numeroSorteado);
+            System.out.println("Você ganhou 10 pontos!");
+        } else if (Math.abs(numeroSorteado - palpiteUsuario) == 1) {
+            System.out.println("Você chegou muito perto mas errou por um");
+            pontuacao += 5;
+            System.out.println("Você ganhou 5 pontos!");
+        } else {
+            System.out.println("Você errou o numero sorteado era: " + numeroSorteado);
+        }
+
+        System.out.println("Pontuação total: " + pontuacao);
+        scanner.close();
+
     }
 }
