c23dbf0772b3ee8171d86809e970879512c8344b
JorgeFVSz <jorge.silvestre@zup.com.br>
2024-12-15 18:30:52 -0300

    feat: implement basic random number generation and display

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int pontuacao = 0;
+
+        int numeroSorteado = random.nextInt(10) + 1;
+        System.out.println("Número sorteado: " + numeroSorteado);
 
     }
 }
